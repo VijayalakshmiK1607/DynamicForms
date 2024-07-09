907e2bd9a120840a25f5d16bd5d7b10536f49c41
VijayalakshmiK <VijayalakshmiK@DESKTOP-QIGH79S>
2024-07-09 23:11:03 +0800

    Db script

--- DBScript.sql
+++ DBScript.sql
@@ -1,8 +1,159 @@
-SELECT TOP (1000) [FieldId]
-      ,[FieldName]
-      ,[FieldType]
-      ,[IsRequired]
-      ,[ValidationRegex]
-      ,[FormID]
-      ,[Value]
-  FROM [DemoForms1].[dbo].[FrmFields]
+﻿ USE [master]
+GO
+/****** Object:  Database [DemoForms1]    Script Date: 3/7/2024 3:00:31 pm ******/
+CREATE DATABASE [DemoForms1]
+ 
+ALTER DATABASE [DemoForms1] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [DemoForms1].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GOa
+ALTER DATABASE [DemoForms1] SET ANSI_NULL_DEFAULT OFF
+GO
+ALTER DATABASE [DemoForms1] SET ANSI_NULLS OFF
+GO
+ALTER DATABASE [DemoForms1] SET ANSI_PADDING OFF
+GO
+ALTER DATABASE [DemoForms1] SET ANSI_WARNINGS OFF
+GO
+ALTER DATABASE [DemoForms1] SET ARITHABORT OFF
+GO
+ALTER DATABASE [DemoForms1] SET AUTO_CLOSE ON
+GO
+ALTER DATABASE [DemoForms1] SET AUTO_SHRINK OFF
+GO
+ALTER DATABASE [DemoForms1] SET AUTO_UPDATE_STATISTICS ON
+GO
+ALTER DATABASE [DemoForms1] SET CURSOR_CLOSE_ON_COMMIT OFF
+GO
+ALTER DATABASE [DemoForms1] SET CURSOR_DEFAULT  GLOBAL
+GO
+ALTER DATABASE [DemoForms1] SET CONCAT_NULL_YIELDS_NULL OFF
+GO
+ALTER DATABASE [DemoForms1] SET NUMERIC_ROUNDABORT OFF
+GO
+ALTER DATABASE [DemoForms1] SET QUOTED_IDENTIFIER OFF
+GO
+ALTER DATABASE [DemoForms1] SET RECURSIVE_TRIGGERS OFF
+GO
+ALTER DATABASE [DemoForms1] SET  ENABLE_BROKER
+GO
+ALTER DATABASE [DemoForms1] SET AUTO_UPDATE_STATISTICS_ASYNC OFF
+GO
+ALTER DATABASE [DemoForms1] SET DATE_CORRELATION_OPTIMIZATION OFF
+GO
+ALTER DATABASE [DemoForms1] SET TRUSTWORTHY OFF
+GO
+ALTER DATABASE [DemoForms1] SET ALLOW_SNAPSHOT_ISOLATION OFF
+GO
+ALTER DATABASE [DemoForms1] SET PARAMETERIZATION SIMPLE
+GO
+ALTER DATABASE [DemoForms1] SET READ_COMMITTED_SNAPSHOT ON
+GO
+ALTER DATABASE [DemoForms1] SET HONOR_BROKER_PRIORITY OFF
+GO
+ALTER DATABASE [DemoForms1] SET RECOVERY SIMPLE
+GO
+ALTER DATABASE [DemoForms1] SET  MULTI_USER
+GO
+ALTER DATABASE [DemoForms1] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [DemoForms1] SET DB_CHAINING OFF
+GO
+ALTER DATABASE [DemoForms1] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF )
+GO
+ALTER DATABASE [DemoForms1] SET TARGET_RECOVERY_TIME = 60 SECONDS
+GO
+ALTER DATABASE [DemoForms1] SET DELAYED_DURABILITY = DISABLED
+GO
+ALTER DATABASE [DemoForms1] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+ALTER DATABASE [DemoForms1] SET QUERY_STORE = OFF
+GO
+USE [DemoForms1]
+GO
+/****** Object:  Table [dbo].[__EFMigrationsHistory]    Script Date: 3/7/2024 3:00:32 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[__EFMigrationsHistory](
+      [MigrationId] [nvarchar](150) NOT NULL,
+      [ProductVersion] [nvarchar](32) NOT NULL,
+ CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY CLUSTERED
+(
+      [MigrationId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Form]    Script Date: 3/7/2024 3:00:32 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Form](
+      [ID] [int] IDENTITY(1,1) NOT NULL,
+      [FormId] [nvarchar](max) NOT NULL,
+      [FormName] [nvarchar](max) NOT NULL,
+      [Description] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_Form] PRIMARY KEY CLUSTERED
+(
+      [ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[FrmFields]    Script Date: 3/7/2024 3:00:32 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[FrmFields](
+      [FieldId] [int] IDENTITY(1,1) NOT NULL,
+      [FieldName] [nvarchar](max) NOT NULL,
+      [FieldType] [nvarchar](max) NOT NULL,
+      [IsRequired] [bit] NOT NULL,
+      [ValidationRegex] [nvarchar](max) NOT NULL,
+      [FormID] [int] NOT NULL,
+      [Value] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_FrmFields] PRIMARY KEY CLUSTERED
+(
+      [FieldId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20240702083855_init', N'8.0.4')
+GO
+SET IDENTITY_INSERT [dbo].[Form] ON
+GO
+INSERT [dbo].[Form] ([ID], [FormId], [FormName], [Description]) VALUES (4, N'Emp', N'EMP', N'11')
+GO
+SET IDENTITY_INSERT [dbo].[Form] OFF
+GO
+SET IDENTITY_INSERT [dbo].[FrmFields] ON
+GO
+INSERT [dbo].[FrmFields] ([FieldId], [FieldName], [FieldType], [IsRequired], [ValidationRegex], [FormID], [Value]) VALUES (3, N'Name', N'text', 1, N'/d', 4, N'VijiMani')
+GO
+INSERT [dbo].[FrmFields] ([FieldId], [FieldName], [FieldType], [IsRequired], [ValidationRegex], [FormID], [Value]) VALUES (4, N'City', N'dropdown', 1, N'/d', 4, N'Trichy,Dindigul')
+GO
+INSERT [dbo].[FrmFields] ([FieldId], [FieldName], [FieldType], [IsRequired], [ValidationRegex], [FormID], [Value]) VALUES (5, N'Name2', N'text', 0, N'/d', 4, N'sfdfd')
+GO
+SET IDENTITY_INSERT [dbo].[FrmFields] OFF
+GO
+/****** Object:  Index [IX_FrmFields_FormID]    Script Date: 3/7/2024 3:00:32 pm ******/
+CREATE NONCLUSTERED INDEX [IX_FrmFields_FormID] ON [dbo].[FrmFields]
+(
+      [FormID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[FrmFields]  WITH CHECK ADD  CONSTRAINT [FK_FrmFields_Form_FormID] FOREIGN KEY([FormID])
+REFERENCES [dbo].[Form] ([ID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[FrmFields] CHECK CONSTRAINT [FK_FrmFields_Form_FormID]
+GO
+USE [master]
+GO
+ALTER DATABASE [DemoForms1] SET  READ_WRITE
+GO
